54a25c8c57e8761706ecf3cbb118c78623237267
ali8569 <wehatexboc360@icloud.com>
2019-06-08 15:02:46 +0430

    some ardunio fixes

--- app/src/androidTest/java/ir/markazandroid/uinitializr/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/ir/markazandroid/uinitializr/ExampleInstrumentedTest.java
@@ -1,13 +1,12 @@
 package ir.markazandroid.uinitializr;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -21,6 +20,15 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("ir.markazandroid.uinitializr", appContext.getPackageName());
+        //assertEquals("ir.markazandroid.uinitializr", appContext.getPackageName());
+
+        Intent intent = new Intent(appContext, TestActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        appContext.startActivity(intent);
+
+        while (true) {
+
+        }
+
     }
 }
